575ad67de04c4557b695a83871d12ca4039fe7e7
Simone Benzi <benhurempire@gmail.com>
2025-04-08 17:39:57 +0200

    refactor(ui): Use DockPanel in ContractView to fix scrolling and layout

--- WpfMvvmApp/Views/ContractView.xaml.cs
+++ WpfMvvmApp/Views/ContractView.xaml.cs
@@ -1,19 +1,14 @@
 // WpfMvvmApp/Views/ContractView.xaml.cs
 using System.Windows.Controls;
-// Rimosse using non necessarie (Input, Media, etc.)
 
 namespace WpfMvvmApp.Views
 {
-    /// <summary>
-    /// Logica di interazione per ContractView.xaml
-    /// </summary>
     public partial class ContractView : UserControl
     {
         public ContractView()
         {
             InitializeComponent();
         }
-
         // NESSUN GESTORE EVENTO O HELPER PER LO SCROLL QUI
     }
 }
--- WpfMvvmApp/Views/LessonView.xaml.cs
+++ WpfMvvmApp/Views/LessonView.xaml.cs
@@ -1,9 +1,11 @@
 // WpfMvvmApp/Views/LessonView.xaml.cs
 using System.Windows.Controls;
-// Rimosse le using non più necessarie per lo scroll (Input, Media) se non usate altrove
 
 namespace WpfMvvmApp.Views
 {
+    /// <summary>
+    /// Logica di interazione per LessonView.xaml
+    /// </summary>
     public partial class LessonView : UserControl
     {
         public LessonView()
@@ -11,7 +13,6 @@ namespace WpfMvvmApp.Views
             InitializeComponent();
         }
 
-        // RIMOSSO il metodo ListView_PreviewMouseWheel
-        // RIMOSSO il metodo FindVisualChild (ora è nel Behavior)
+        // NESSUN GESTORE EVENTO O HELPER PER LO SCROLL QUI
     }
 }
